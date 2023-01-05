4a7ca8012737dbbcdb70cab110014b3db4c6b875
grandeto <grandeto.com@gmail.com>
2023-01-05 21:14:41 +0200

    add sort and generate services unit tests

--- test/generate_service_test.erl
+++ test/generate_service_test.erl
@@ -7,7 +7,8 @@ generate_test_() ->
      fun setup/0,
      fun teardown/1,
      [
-      {"Generate data", fun test_generate_data/0}
+      {"Generate command1", fun test_generate_command1/0},
+      {"Generate command2", fun test_generate_command2/0}
      ]
     }.
 
@@ -17,6 +18,18 @@ setup() ->
 teardown(_) ->
     ok.
 
-test_generate_data() ->
-    Data = #{id => 1},
-    ?assertEqual({ok, #{<<"Generated">> => Data}}, generate_service:generate(Data)).
+test_generate_command1() ->
+    {ok, Task} = file:read_file(<<"./priv/tasks-1.json">>),
+    Json = jsx:decode(Task, [return_maps]),
+    Tasks = maps:get(<<"tasks">>, Json),
+    {ok, Command} = generate_service:generate(Tasks, sort_service),
+    Expected = <<"touch file1\necho 'Hello World!' > file1\ncat file1\nrm file1\n">>,
+    ?assertEqual(Command, Expected).
+
+test_generate_command2() ->
+    {ok, Task} = file:read_file(<<"./priv/tasks-2.json">>),
+    Json = jsx:decode(Task, [return_maps]),
+    Tasks = maps:get(<<"tasks">>, Json),
+    {ok, Command} = generate_service:generate(Tasks, sort_service),
+    Expected = <<"touch file2\necho 'Hello World!' > file2\ncat file2\nls -lah file2\npwd\nrm file2\n">>,
+    ?assertEqual(Command, Expected).
--- test/sort_service_test.erl
+++ test/sort_service_test.erl
@@ -7,7 +7,8 @@ sort_test_() ->
      fun setup/0,
      fun teardown/1,
      [
-      {"Sort data", fun test_sort_data/0}
+      {"Sort tasks-1", fun test_sort_tasks1/0},
+      {"Sort tasks-2", fun test_sort_tasks2/0}
      ]
     }.
 
@@ -17,6 +18,40 @@ setup() ->
 teardown(_) ->
     ok.
 
-test_sort_data() ->
-    Data = #{id => 1},
-    ?assertEqual({ok, #{<<"Sorted">> => Data}}, sort_service:sort(Data)).
+test_sort_tasks1() ->
+    {ok, Task} = file:read_file(<<"./priv/tasks-1.json">>),
+    Json = jsx:decode(Task, [return_maps]),
+    Tasks = maps:get(<<"tasks">>, Json),
+    {ok, Sorted} = sort_service:sort(Tasks),
+    Expected = [
+        #{<<"command">> => <<"touch file1">>,
+            <<"name">> => <<"task-1">>},
+        #{<<"command">> => <<"echo 'Hello World!' > file1">>, 
+            <<"name">> => <<"task-3">>},
+        #{<<"command">> => <<"cat file1">>, 
+            <<"name">> => <<"task-2">>},
+        #{<<"command">> => <<"rm file1">>, 
+            <<"name">> => <<"task-4">>}
+      ],
+    ?assertEqual(Sorted, Expected).
+
+test_sort_tasks2() ->
+    {ok, Task} = file:read_file(<<"./priv/tasks-2.json">>),
+    Json = jsx:decode(Task, [return_maps]),
+    Tasks = maps:get(<<"tasks">>, Json),
+    {ok, Sorted} = sort_service:sort(Tasks),
+    Expected = [
+        #{<<"command">> => <<"touch file2">>,
+            <<"name">> => <<"task-1">>},
+        #{<<"command">> => <<"echo 'Hello World!' > file2">>, 
+            <<"name">> => <<"task-3">>},
+        #{<<"command">> => <<"cat file2">>, 
+            <<"name">> => <<"task-5">>},
+        #{<<"command">> => <<"ls -lah file2">>, 
+            <<"name">> => <<"task-2">>},
+        #{<<"command">> => <<"pwd">>, 
+            <<"name">> => <<"task-4">>},
+        #{<<"command">> => <<"rm file2">>,
+            <<"name">> => <<"task-6">>}
+      ],
+    ?assertEqual(Sorted, Expected).
